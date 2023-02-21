7f9126f15f3ffdad20879ddc719e2603ffd5f110
Mehdi2707 <mehdi2707@live.fr>
2023-02-21 19:11:08 +0100

    first commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct( private AdminUrlGenerator $adminUrlGenerator )
+    {
+        
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator->setController(ArticleCrudController::class)
+                                       ->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony CMS');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToRoute('Retour sur le site', 'fa fa-undo', 'app_home');
+        yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+            MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
+            MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class)
+        ]);
+        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+
+    }
+}

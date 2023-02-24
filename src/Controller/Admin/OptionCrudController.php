1666960d27dbc3ad470c4abeb38dac7b38c6a020
Mehdi2707 <mehdi2707@live.fr>
2023-02-24 18:29:14 +0100

    config install app

--- src/Controller/Admin/OptionCrudController.php
+++ src/Controller/Admin/OptionCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Option;
+use App\Repository\OptionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -11,6 +15,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\HiddenField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -20,9 +25,19 @@ use Symfony\Component\HttpFoundation\Response;
 
 class OptionCrudController extends AbstractCrudController
 {
+    public function __construct(private OptionRepository $optionRepo)
+    {
+        
+    }
+
     public static function getEntityFqcn(): string
     {
         return Option::class;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        return $this->optionRepo->getIndexQueryBuilder();
     }
 
     public function index(AdminContext $context)
